87b961a7ffe9e5bb9846893cb01c2e67cdb03c91
Raghavendra Prabhu <ragh@RaghavedrasiMac.home>
2020-07-09 17:15:12 -0700

    Adding favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -91,6 +91,26 @@ module.exports = {
       },
     },
     `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        //trackingId: `ADD YOUR TRACKING ID HERE`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `JumpML`,
+        short_name: `JumpML`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#ffffff`,
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: `standalone`,
+        icon: `static/jumpML.svg`, // This path is relative to the root of the site.
+      },
+    },
     `gatsby-plugin-sharp`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-postcss`,
